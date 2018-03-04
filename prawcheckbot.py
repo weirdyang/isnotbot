6b224498de143761395ab968c9b2a4ff1c99d490
captmomo <wy4786@gmail.com>
2018-03-04 14:51:46 +0800

    Update prawcheckbot.py

--- prawcheckbot.py
+++ prawcheckbot.py
@@ -1,18 +1,22 @@
 import learningbot
+import praw
 
-from prawbot import login_reddit, answers_scrape
 from similarity import cosine_sim
 
 
 def main():
     reddit = login_reddit()
-    user = 'skoetje'
-    comments_list = reddit.redditor(user).comments.new(limit=10)
+    user = 'remindmebot'
+    comments_list = reddit.redditor(user).comments.new(limit=5)
     input_list = [comment.body for comment in comments_list]
     #print(input_list)
+    learningbot.testing_code(input_list)  
     learningbot.pipeline_cls(input_list)
-    learningbot.testing_code(input_list)
     print(cosine_sim(input_list))
+
+def login_reddit():
+    reddit = praw.Reddit('isnotbot', user_agent='<Python> Test bot by u/captmomo')
+    return reddit
 
 if __name__ == '__main__':
     main()
