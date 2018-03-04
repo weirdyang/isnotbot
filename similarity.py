52400be1c086226cdcb718f662d80a1b822ee1bf
captmomo <wy4786@gmail.com>
2018-03-04 14:44:16 +0800

    Create similarity.py

---
+++ similarity.py
@@ -0,0 +1,25 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#https://machinelearningmastery.com/clean-text-machine-learning-python/
+###https://stackoverflow.com/questions/8897593/similarity-between-two-text-documents###
+stop_words = stopwords.words('english')
+twttokenizer = TweetTokenizer(preserve_case=False)
+stemmer = nltk.PorterStemmer()
+
+
+def stem_tokens(tokens):
+    return [stemmer.stem(word) for word in tokens]
+
+
+def normalize(text):
+    answer = text.splitlines()
+    comment = ' '.join(answer)
+    return stem_tokens(twttokenizer.tokenize(comment))
+
+
+def cosine_sim(list_of_text):
+    vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
+    tfidf = vectorizer.fit_transform(list_of_text)
+    return ((tfidf * tfidf.T).A)[0, 1]
