dc93d14d8fa6c2fb38abfc74d3103d6a76f162ba
captmomo <wy4786@gmail.com>
2018-03-04 14:09:29 +0800

    Update prawcheckbot.py

--- prawcheckbot.py
+++ prawcheckbot.py
@@ -6,7 +6,6 @@ from similarity import cosine_sim
 
 def main():
     reddit = login_reddit()
-    url = 'https://www.reddit.com/r/AskReddit/comments/7zzim3/what_app_is_so_useful_you_cant_believe_its_free/'
     user = 'skoetje'
     comments_list = reddit.redditor(user).comments.new(limit=10)
     input_list = [comment.body for comment in comments_list]
